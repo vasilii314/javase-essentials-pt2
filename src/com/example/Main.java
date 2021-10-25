af095a913ff6bc885c99561f2b51fd2db74d0082
vasilii314 <pythagoras3145926@gmail.com>
2021-10-25 20:13:21 +0400

    Main corrected

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -8,15 +8,22 @@ public class Main {
 
     public static void main(String[] args) {
 	    // 1
-        var emp = new Employee("John", "Doe", 35) {
+        var emp = new Employee("Anonymous", "Employee", 35) {
             @Override
             public void work() {
                 System.out.println("ANONYMOUS_EMPLOYEE: doing something...");
             }
+
+            public void takeABreak() {
+                System.out.println("ANONYMOUS_EMPLOYEE: sleeping...");
+            }
         };
+
+        emp.takeABreak();
 
         // 2
         List<Employee> employees = new ArrayList<>();
+        employees.add(emp);
         employees.add(new Developer("John", "Doe", 30, 7, true));
         employees.add(new Intern("Meryl", "Streep", 72, 40, true));
         employees.add(new TeamLead("Miranda", "Priestly", 45, 20, true));
